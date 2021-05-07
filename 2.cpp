6ad4d8c86ec5452df5fb0df030044a4f853572c1
dennis-max <57693915+dennis-max@users.noreply.github.com>
2021-05-07 12:09:05 +0800

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+typedef struct data
+{
+    int ans;
+    char c;///¦r¥À
+}DATA;
+///struct data box;
+DATA box;
+int main()
+{
+    box.ans=1;
+    box.c='A';
+
+    printf("%c %d\n", box.c, box.ans );
+}
